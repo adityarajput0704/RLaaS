c5b7bf844c142da347892d810d49d23d467e4e53
Aditya Rajput <rajput.aditya.tech@gmail.com>
2026-07-28 19:42:51 +0530

    Implemented fixed window counter algorithm

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 from fastapi import FastAPI
+import time
 
 app = FastAPI()
+
+
+
+class FixedWindowLimiter:
+    def __init__(self, limit:int, window_size:int):
+        self.limit = limit
+        self.window_size = window_size
+        self.request_counts = {}
+        self.window_start_time = {}
+
+    def is_request_allowed (self, user_id:str):
+        current_time = time.time()
+        if user_id not in self.request_counts:
+            self.request_counts[user_id] = 1
+            self.window_start_time[user_id] = current_time
+            return {"status": "Request successful."}
+        
+        if(current_time - self.window_start_time[user_id] > self.window_size):
+            self.request_counts[user_id] = 1
+            self.window_start_time[user_id] = current_time
+            return {"status": "Request successful."}
+
+        if(self.request_counts[user_id] > self.limit):
+            return {"status": "Rate limit exceeded. Please try again later."}
+
+        self.request_counts[user_id] += 1
+        print(f"Current: {current_time}")
+        print(f"Start: {self.window_start_time.get(user_id)}")
+        print(f"Elapsed: {current_time - self.window_start_time.get(user_id, current_time)}")
+        print(f"Count: {self.request_counts.get(user_id)}")
+        return {"status": "Request successful."}
+
+
+limiter = FixedWindowLimiter(limit=5, window_size=60)
 
 @app.get('/')
 def home():
     return{"status": "System is Running Successfully"}
 
 
-request_counts = {}
 @app.get('/rate_limiter')
 def rate_limiter():
     
     user_id = "user_145"  # Example user ID
-    limit = 5  # Maximum number of requests allowed
+    return limiter.is_request_allowed(user_id)
 
-    request_counts[user_id]= request_counts.get(user_id, 0)+1;
-
-    if request_counts[user_id] > limit:
-        return {"status": "Rate limit exceeded. Please try again later."}
-    
-    return {"status": "Request successful."}
