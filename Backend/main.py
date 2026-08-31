e7285b8eb268c854eabfd8b1055fcdccce439795
Aditya Rajput <rajput.aditya.tech@gmail.com>
2026-08-31 18:37:35 +0530

    modified cors origin

--- Backend/main.py
+++ Backend/main.py
@@ -19,7 +19,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://rlaas.netlify.app",
+                   "https://6a957ad808adfa76daad1429--rlaas.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
